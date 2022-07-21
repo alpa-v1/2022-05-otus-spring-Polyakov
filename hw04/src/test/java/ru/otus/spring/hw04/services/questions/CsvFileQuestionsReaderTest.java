1f96e0651764b9175882898dd69cfe47061c3599
alpa-v1 <alpa.v1@pm.me>
2022-07-21 23:50:27 +0300

    [hw-04] - moved application to spring shell

---
+++ hw04/src/test/java/ru/otus/spring/hw04/services/questions/CsvFileQuestionsReaderTest.java
@@ -0,0 +1,43 @@
+package ru.otus.spring.hw04.services.questions;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import ru.otus.spring.hw04.domain.Question;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.ArgumentMatchers.anyList;
+import static org.mockito.BDDMockito.given;
+import static ru.otus.spring.hw04.util.QuestionsDataGenerator.getQuestions;
+
+@SpringBootTest
+@DisplayName("Given csv file questions reader")
+class CsvFileQuestionsReaderTest {
+
+    @MockBean
+    private ResourceFileReader resourceFileReader;
+
+    @MockBean
+    private QuestionsParser questionsParser;
+
+    @Autowired
+    private QuestionsReader csvFileQuestionsReader;
+
+    @Test
+    @DisplayName("when csv file is present, then read and return all questions as objects")
+    void when_CsvFileIsPresent_then_ReadAndReturnAllQuestionsAsObjects() {
+        // given
+        given(questionsParser.parse(anyList())).willReturn(getQuestions());
+
+        // when
+        List<Question> questions = csvFileQuestionsReader.read();
+
+        // then
+        List<Question> expectedQuestions = getQuestions();
+        assertThat(questions).isEqualTo(expectedQuestions);
+    }
+}
